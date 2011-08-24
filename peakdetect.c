0fc6e64a4b4dadcd7bde918bea9da3edc16fd3b1
Hong Xu <xuphys@gmail.com>
2011-08-24 12:39:08 +0800

    Change license to FreeBSD license.

--- peakdetect.c
+++ peakdetect.c
@@ -1,11 +1,36 @@
 /*
- * Peak detection in a wave.
+ * Copyright 2011 Hong Xu. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *   1. Redistributions of source code must retain the above copyright notice,
+ *      this list of conditions and the following disclaimer.
+ *
+ *   2. Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY HONG XU ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ * EVENT SHALL HONG XU OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and documentation are
+ * those of the authors and should not be interpreted as representing official
+ * policies, either expressed or implied, of Hong Xu.
+ *
+ *
+ *
+ * Purpose: Peak detection in a wave.
  *
  * Author: Hong Xu <xuphys@gmail.com>
- *
- * This software is released under GPLv3:
- *
- * http://www.gnu.org/licenses/gpl-3.0.html
  *
  */
 #include <stdio.h>
@@ -48,7 +73,9 @@ void print_version(void)
             "peakdetect version 0.1.1\n"
             "Copyright (C) 2011 Hong Xu <xuphys@gmail.com>\n"
             "Originally inspired by Eli Billauer\'s peakdet for MATLAB:\n"
-            "http://billauer.co.il/peakdet.html\n");
+            "http://billauer.co.il/peakdet.html\n"
+            "\n"
+            "See the README file for license information.\n");
     exit(0);
 }
 
